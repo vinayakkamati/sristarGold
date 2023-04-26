bef441ba8ea9476ce374fb52d17e5968a43e524b
vinayak kamati <vinayakkamati123@gmail.com>
2023-04-26 11:47:44 +0530

    Initial commit

---
+++ src/main/java/com/gold/sristarGold/SristarGoldApplication.java
@@ -0,0 +1,13 @@
+package com.gold.sristarGold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SristarGoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SristarGoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gold/sristarGold/SristarGoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gold.sristarGold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SristarGoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
